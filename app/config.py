fa7b094da26c8d1590e7f9595a9fae2fe38365a7
07gaura <gouravlok@gmail.com>
2023-07-03 16:51:13 +0530

    Added Dockerfile

--- app/config.py
+++ app/config.py
@@ -10,7 +10,6 @@ class BaseConfig(object):
     password = "QWEasdzxc123"
     dbname = "krishig_db"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@65.0.252.8:3307/{dbname}"
-
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'heythisisthesecretkey'
 
